7bcf24c782b4e8f1fa3c03e2601f28f3d2715998
TravisSLee <travisslee223@gmail.com>
2021-03-09 18:24:45 -0500

    update model and add js files

--- backend/app/controllers/countries_controller.rb
+++ backend/app/controllers/countries_controller.rb
@@ -1,7 +1,37 @@
 class CountriesController < ApplicationController
+    
+    
+    def index
+        countries = Country.all
+    end
 
+    def  show
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def delete
+
+    end
 
     private
 
-    def 
+    def  set_entry
+        entry = Entry.find_by(id: params[:id])
+    end
+
+    def set_country
+        country = Country.find_or_create_by(id: params[:country_id])
+    end
+
+    def countries_params
+        params.require(:country).permit(:name, :area, :activity)
+    end
 end
--- backend/app/controllers/entries_controller.rb
+++ backend/app/controllers/entries_controller.rb
@@ -1,7 +1,31 @@
 class EntriesController < ApplicationController
- 
+    before_action :set_entry
+    
+    def index
+        entries = Entry.all
+    end
+
+    def  show
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
 
     private
+
+    def  set_entry
+        entry = Entry.find_by(id: params[:id])
+    end
+
+    def set_country
+        country = Country.find_or_create_by(id: params[:country_id])
+    end
 
     def entries_params
         params.require(:entry).permit( :country_id, :title, :content, :favorite, :start_date, :end_date, country_attributes: [
